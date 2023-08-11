302733144b72672170d3987a0189c8c759eea46f
Sofa Fatuk <sofa111298@gmail.com>
2023-08-11 16:03:56 +0400

    Add frontend

--- index.ts
+++ index.ts
@@ -7,6 +7,7 @@ import router from "./src/db/router";
 import syncRepos from "./src/syncRepos";
 
 import * as dotenv from "dotenv";
+import path from "path";
 
 dotenv.config();
 
@@ -19,6 +20,8 @@ const DB_URL = process.env.DB_URL;
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "frontend", "build")));
+
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/api", router);
@@ -29,7 +32,9 @@ app.get("/", (req: Request, res: Response) => {
 
 async function startApp() {
   try {
-    console.log("DB_URL", DB_URL);
+    if (!DB_URL) {
+      throw new Error("No DB_URL");
+    }
     await mongoose.connect(DB_URL);
     await syncRepos();
     app.listen(PORT, () => console.log("hello"));
